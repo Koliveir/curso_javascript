a88315bcf07ea3dd878d14e0fcdef7ae9b3cc0a3
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-07 22:14:01 -0300

    Add files via upload

---
+++ aula_13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log(`Tudo bem? ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}*/
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
---
+++ aula_14/ambiente.js
@@ -0,0 +1,16 @@
+/*var c = 1
+
+while (c<=5) {
+    console.log(`loop ${c}`)
+    c++
+}*/
+
+/*do {
+    console.log(`loop ${c}`)
+    c++
+} while (c <= 5)*/
+console.log('Inicio')
+for (var c = 1; c <= 10; c++) {
+    console.log(`loop ${c}`)
+}
+console.log('Fim!')
