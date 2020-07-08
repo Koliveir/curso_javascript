e393945acbf0fedf9fb0880ae8722cc7bb5d1c9f
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-07 22:01:57 -0300

    Arquivos que já finalizei

---
+++ aula_11/ex008.js
@@ -0,0 +1,11 @@
+//console.log('Hello, world!')
+
+var vel = 70
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
