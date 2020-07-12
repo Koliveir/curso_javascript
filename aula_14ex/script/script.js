171f08c89f1ccaaba6dd814fcbaf5ef2f8c64328
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-12 14:15:17 -0300

    Script funcionando

--- aula_14ex/script/script.js
+++ aula_14ex/script/script.js
@@ -1,18 +1,25 @@
-/*var inicio = 1
-var fim = 100
-var passo = 1
-
-while (inicio <= fim) {
-    console.log(`Loop ${inicio}`)
-    inicio += passo
-}*/
-
-var inicio = window.document.getElementById('inicio')
-var fim = window.document.getElementById('fim')
-var salto = window.document.getElementById('salto')
-var res = window.document.getElementById('res')
-
-while (inicio <= fim) {
-    res.innerHTML=`contagem ${inicio}`
-    inicio+=salto
-}
+function contagem() {
+    var inicio = window.document.getElementById('inicio')
+    var ini = Number(inicio.value)
+    var fim = window.document.getElementById('fim')
+    var fim_f = Number(fim.value)
+    var salto = window.document.getElementById('salto')
+    var sal = Number(salto.value)
+    var res = window.document.getElementById('res')
+    //res.innerHTML=""
+    if (sal == 0) {
+        window.alert('Verfique o número de salto para não causar loop')
+    } else if  (ini < fim_f) {
+            while (ini <= fim_f) {
+                res.innerHTML += `<p>&#x1F449 ${ini}, </p>`
+                ini+=sal
+            }
+        } else if (fim_f <= ini) {
+            while (fim_f <= ini) {
+                res.innerHTML += `<p>&#x1F449 ${ini}, </p>`
+                ini-=sal
+            }
+        }
+        //res.innerHTML = '<p>&#x1FAC1</p>'
+    }
+    
